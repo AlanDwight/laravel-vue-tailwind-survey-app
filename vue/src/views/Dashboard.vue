c2370cd7dd9133f5bc645a608930659bbd605f30
AlanDwight <winkyawphyo78@gmail.com>
2025-05-15 16:40:49 +0630

    initial commit

---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <h1 class="text-2xl text-yellow-400 flex justify-center items-center h-[16rem] bg-black ">Dashboard</h1>
+</template>
+
+<style scoped></style>
